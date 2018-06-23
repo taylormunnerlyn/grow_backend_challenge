29f9328ecb797b871a64c15ebecd822976f272a8
Taylor Munnerlyn <taylor@Admins-MacBook-Pro.local>
2018-06-23 00:33:47 -0700

    finishing functionality of both endpionts

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const request = require('request');
-const cors    = require('cors');
+const request = require('request-promise');
+var Promise   = require('promise');
 const app     = express();
 
 const port = 3000;
@@ -15,145 +15,228 @@ app.get('/planets',
     jsonResponse
 );
 
-var sortByArray = ['name', 'height', 'mass'];
-
-var queryParam = '';
-
-var resultsToShow = [];
-
+const sortByArray = ['name', 'height', 'mass'];
+var swapiData = [];
 var url = '';
-
 var continueLooping = false;
+var doneLooping = false;
 
-var origRes = '';
-
-function getSwapiData() {
-    return new Promise((resolve, reject) => {
-        request(url, function(err, res, body){
-        // request("https://swapi.co/api/people/?page=9", function(err, res, body){
-            // console.log(JSON.parse(body));
-
-            if (err || body[0] === '<') {
-                res.locals = {
-                    success: false,
-                    error: err || 'Invalid request.'
-                };
-                jsonResponse(res);
-            }
-
-            for (var value of JSON.parse(body).results) {
-                // console.log(value);
-                resultsToShow.push(value);
-              }
-            // resultsToShow.push(JSON.parse(body).results);
-            var next_url = JSON.parse(body).next;
-            console.log("next_url:");
-            console.log(next_url);
-            if (next_url) {
-                console.log("keep going");
-                url = next_url;
-                continueLooping = true;
-            } else {
-                console.log("stop");
-                url = "";
-                continueLooping = false;
-            }
-            console.log("continue looping?");
-            console.log(continueLooping);
-            resolve("SUCCESS");
-        });
-    });
-}
-
-function looper(res) {
-    this_res = res;
-    const data = getSwapiData();
-    data.then(function(res) {
-        if (continueLooping) {
-            looper(this_res);
-        } else {
-            // console.log("resultsToShow:");
-            // console.log(resultsToShow);
-
-            console.log("resultsToShow length:");
-            console.log(resultsToShow.length);
-
-            console.log("done looping");
-
-            handleApiResponse(this_res);
-        }
-    })
-    .catch((error) => {
-        console.log(error, 'error');
-        // res.locals = {
-        //     success: false,
-        //     error: error || 'Promise error'
-        // };
-        // jsonResponse(res);
-    });
-}
-
-function getRequestData(req, res) {
-
-    if (req.url === '/people') {
-        url = `https://swapi.co/api/people/`;
-    } else if (req.url === '/planets') {
-        url = `https://swapi.co/api/planets/`;
-    } else {
-        return "Invalid Request URL.";
-    }
+function getRequestData(req, res, next) {
 
     if (req.query.sortBy) {
-        queryParam = req.query.sortBy
+        var queryParam = req.query.sortBy
     }
 
-    looper(res);
-}
+    function getSwapiData() {
+        return new Promise((resolve, reject) => {
+            request(url, function(err, res, body){
 
-function handleApiResponse(res) {
+                if (err || body[0] === '<') {
+                    res.locals = {
+                        success: false,
+                        error: err || 'Invalid request.'
+                    };
+                    return next();
+                }
 
-    if (sortByArray.includes(queryParam)) {
+                for (var value of JSON.parse(body).results) {
+                    swapiData.push(value);
+                }
 
-        function compareNames(a, b) {
-            var nameA = a.name.toUpperCase(); // ignore upper and lowercase
-            var nameB = b.name.toUpperCase(); // ignore upper and lowercase
-            if (nameA < nameB) {
-                return -1;
-            }
-            if (nameA > nameB) {
-                return 1;
-            }
+                if (JSON.parse(body).next) {
+                    url = JSON.parse(body).next;
+                    continueLooping = true;
+                } else {
+                    url = "";
+                    continueLooping = false;
+                    doneLooping = true;
+                }
 
-            // names must be equal
-            return 0;
-        };
+                resolve("SUCCESS");
 
-        function compareHeight(a, b) {
-            return a.height - b.height;
-        };
+            });
+        });
+    }
 
-        function compareMass(a, b) {
-            return a.mass - b.mass;
-        };
+    if (req._parsedUrl.pathname === '/people') {
+        url = `https://swapi.co/api/people/`;
 
-        if (queryParam === 'name') {
-            resultsToShow.sort(compareNames);
-        } else if (queryParam === 'height') {
-            resultsToShow.sort(compareHeight);
-        } else {
-            resultsToShow.sort(compareMass);
+        function peopleLooper(){
+            getSwapiData().then(function() {
+
+                if (continueLooping) {
+                    peopleLooper();
+                }
+
+                if (doneLooping) {
+
+                    if (queryParam && sortByArray.includes(queryParam)) {
+                
+                        function compareNames(a, b) {
+                            var nameA = a.name.toUpperCase();
+                            var nameB = b.name.toUpperCase();
+                            if (nameA < nameB) {
+                                return -1;
+                            }
+                            if (nameA > nameB) {
+                                return 1;
+                            }
+                
+                            return 0;
+                        };
+                
+                        function compareHeight(a, b) {
+                            return a.height - b.height;
+                        };
+                
+                        function compareMass(a, b) {
+                            return a.mass - b.mass;
+                        };
+                
+                        if (queryParam === 'name') {
+                            swapiData.sort(compareNames);
+                        } else if (queryParam === 'height') {
+                            swapiData.sort(compareHeight);
+                        } else {
+                            swapiData.sort(compareMass);
+                        }
+                    }
+        
+        
+                    res.locals = {
+                        success: true,
+                        results: swapiData
+                    };
+        
+                    swapiData = [];
+                    url = '';
+                    continueLooping = false;
+                    doneLooping = false;
+
+                    return next();
+                }
+            })
+            .catch((error) => {
+                res.locals = {
+                    success: false,
+                    error: error || 'Invalid request.'
+                };
+                return next();
+            });
         }
+        peopleLooper();
+
+    } else if (req._parsedUrl.pathname === '/planets') {
+        url = `https://swapi.co/api/planets/`;
+
+        async function getResidentData(residentUrl) {
+
+            try {
+                var data = await request(residentUrl);
+            } catch(error) {
+                res.locals = {
+                    success: false,
+                    error: error || 'Invalid request.'
+                };
+                return next();
+            }
+
+            return data;
+
+        }
+
+        async function residentLooper(planetIndex) {
+            for (var [residentIndex, thisResident] of swapiData[planetIndex].residents.entries()) {
+
+                try {
+                    var data = await getResidentData(thisResident)
+                } catch(error) {
+                    res.locals = {
+                        success: false,
+                        error: error || 'Invalid request.'
+                    };
+                    return next();
+                }
+
+                swapiData[planetIndex].residents[residentIndex] = JSON.parse(data).name
+
+            }
+        }
+
+        async function planetLooper() {
+            for (var [planetIndex, thisPlanet] of swapiData.entries()) {
+
+                if (thisPlanet.residents.length > 0) {
+
+                    try {
+                        await residentLooper(planetIndex);
+                    } catch(error) {
+                        res.locals = {
+                            success: false,
+                            error: error || 'Invalid request.'
+                        };
+                        return next();   
+                    }
+
+                    return new Promise((resolve, reject) => {resolve('success');});
+
+                }
+            } 
+        }
+
+        function handlePlanetData(){
+            getSwapiData().then(function() {
+                if (continueLooping) {
+                    handlePlanetData();
+                }
+
+                if (doneLooping) {
+                    planetLooper().then(function() {
+
+                        res.locals = {
+                            success: true,
+                            results: swapiData
+                        };
+            
+                        swapiData = [];
+                        url = '';
+                        continueLooping = false;
+                        doneLooping = false;
+
+                        return next();
+
+                    })
+                    .catch((error) => {
+                        res.locals = {
+                            success: false,
+                            error: error
+                        };
+                        return next();
+                    });
+                }
+            })
+            .catch((error) => {
+                res.locals = {
+                    success: false,
+                    error: error || 'Invalid request.'
+                };
+                return next();
+            });
+        }
+        handlePlanetData();
+
+    } else {
+        res.locals = {
+            success: false,
+            error: new Error("Invalid request.")
+        };
+        return next();
     }
-    res.locals = {
-        success: true,
-        results: resultsToShow
-    };
-    jsonResponse(res);
 }
 
-function jsonResponse(res) {
+function jsonResponse(req, res, next) {
     return res.json(res.locals);
-}
+  }
 
 app.listen(port, function () {
     console.log("Server is running on "+ port +" port");
